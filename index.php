e444c9b29621410308c6681158ceed1e7c9043e1
najia01 <validress@outlook.fr>
2023-08-22 14:25:49 +0200

    begin homepage

--- index.php
+++ index.php
@@ -1,44 +1,119 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PROMO 180</title>
     <link rel="stylesheet" href="./asset/css/style.css">
-    
+
+
+    <title>PROMO 180</title>
+
 </head>
+
 <body>
-<main>
-<header>
 
-        <div id="logoBurger">
-            <img id="logo" src="./asset/img/logo.png" alt="logo">
-            <img id="menuBurger" src="./asset/img/menu.png" alt="menu">
+    <main>
+
+        <header>
+
+            <div id="logoBurger">
+                <img id="logo" src="./asset/img/logo.png" alt="logo">
+                <img id="menuBurger" src="./asset/img/menu.png" alt="menu">
+            </div>
+            <nav>
+                <ul id="navigation">
+                    <li><a href="#">Catégorie</a>
+                        <ul class="sub-categories">
+                            <li><a href="#">Technologie</a></li>
+                            <li><a href="#">Outils</a></li>
+                            <li><a href="#">Astuces</a></li>
+
+                    </li>
+                </ul>
+                <ul>
+                    <li><a href="#">Actualité</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                </ul>
+            </nav>
+
+        </header>
+
+        <div class="hero">
+            <img src="./asset/img/groupe.JPG" alt="groupe de stagiaire">
+
+            <p class="presentation">Découvrez l'univers passionnant de la PROMO 180 de stagiaires année 2022/2023 en développement web et web mobiles d'OnlineFormaPro !<br> Plongez-vous dans un monde captivant où la technologie et la créativité fusionnent pour créer des expériences uniques.<br> Notre groupe est composé de personnes venant de différents horizons, tous liés par leur passion pour la création en ligne.<br> Rejoignez-nous pour suivre les aventures palpitantes de notre PROMO 180 et explorer cet excitant voyage au cœur de l'innovation digitale !</p>
         </div>
-        <nav>
-            <ul id="navigation">
-                <li><a href="#">Catégorie</a>
-                    <ul class="sub-categories">
-                        <li><a href="#">Technologie</a></li>
-                        <li><a href="#">Outils</a></li>
-                        <li><a href="#">Astuces</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <ul><li><a href="#">Actualité</a></li></ul>
-            <ul><li><a href="#">Contact</a></li></ul>
-        </nav>
 
-    </header>
+        <section class="cardStudent">
 
-    <div class="hero">
-        <img src="./asset/img/groupe.JPG" alt="groupe de stagiaire">
+            <h2 class="title-recommand">Stagiaires 2022/2023</h2>
+            <div class="wrapper">
+                <div class="card">
+                    <input type="checkbox" id="card1" class="more">
+                    <div class="content">
+                        <div class="front" style="background-image: url('./asset/img/najia.JPG');">
+                            <div class="inner">
+                                <h2>Najia LIGALI</h2>
+                                <label for="card1" class="button" aria-hidden="true">
+                                    Détails
+                                </label>
+                            </div>
 
-        <p class="presentation">Découvrez l'univers passionnant de la PROMO 180 de stagiaires année 2022/2023 en développement web et web mobiles d'OnlineFormaPro ! Plongez-vous dans un monde captivant où la technologie et la créativité fusionnent pour créer des expériences uniques. Notre groupe  est composé de personnes venant de différents horizons, tous liés par leur passion pour la création en ligne. Rejoignez-nous pour suivre les aventures palpitantes de notre PROMO 180 et explorer cet excitant voyage au cœur de l'innovation digitale !</p>
-    </div>
-</main>
+                        </div>
+                        <div class="back">
+                            <div class="inner">
+                               
+                                <div class="info">
+                                    <span id="titleSpan">Langage</span>
+                                    <span>HTML,CSS,JS,PHP,PYTHON</span>
+
+                                </div>
+                                <div class="info">
+                                    <span id="titleSpan">Outils</span>
+                                    <span>VSCode,CodePen,Github,ChatGPT,Notion,Figma,Trello</span>
+
+                                </div>
+
+                                <div class="description">
+                                    <p id="titleSpan">Mes liens <br>
+                                        </p>
+                                    <a href="http://fr.linkedin.com/"><img src="./asset/img/linkedin.png" alt="compte linkedin"></a><br>
+                                    <a href="http://github.com/najia01/portfolio"></a>
+
+                                </div>
+
+                                <label for="card1" class="button return" aria-hidden="true">
+                                    <span>Retour</span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
-<script src="./asset/main.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </section>
+    </main>
+
+
+    <script src="./asset/main.js"></script>
 </body>
+
 </html>
