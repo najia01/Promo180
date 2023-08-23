c7f620a71fb6361e7565d6f15b10f83ca6bf4d36
najia01 <validress@outlook.fr>
2023-08-23 15:58:12 +0200

    error404ok

---
+++ error404.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/css/error404.css">
+
+    <title>CATEGORIES</title>
+</head>
+
+<body>
+    <main>
+        <!-- *-*-*-*-*-*HEADER*-*-*-*-*-* -->
+        <header>
+
+            <div id="logoBurger">
+                <a href="./index.php"><img id="logo" src="./asset/img/logo.png" alt="logo"></a>
+                <img id="menuBurger" src="./asset/img/menu.png" alt="menu">
+            </div>
+            <nav>
+                <ul id="navigation">
+                    <li><a href="./category.php">Catégorie</a>
+
+                    </li>
+
+                    <ul>
+                        <li><a href="./actuality.php">Actualité</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="./contact.php">Contact</a></li>
+                    </ul>
+                </ul>
+            </nav>
+
+        </header>
+
+        <!-- *-*-*-*-*-*-*-ERROR 404*-*-*-*-*-*-*- -->
+
+        <section class="error">
+
+            <img id="erreur" src="./asset/img/error.jpg" alt="erreur 404">
+
+            <div class="button-container">
+            <a href="./index.php" class="button">Retour à l'accueil</a>
+            </div>
+            
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <footer>
+
+            <img id="footerLogo" src="./asset/img/logo.png" alt="logo promo">
+            <span class="promo">PROMO 180</span>
+            <div class="copyright">
+                <p id="copy">Copyright&copy;2023 Najia</p>
+            </div>
+
+
+
+        </footer>
+
+
+
+
+    </main>
+
+    <script src="./asset/main.js"></script>
+</body>
+
+</html>
